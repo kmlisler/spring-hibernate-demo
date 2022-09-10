b17438f987c9bf4ddfc3e1e1be4e739040bb2b26
kmlisler <82888052+kmlisler@users.noreply.github.com>
2022-09-10 20:51:09 +0300

    learning bean singleton and prototype scope

---
+++ src/com/luv2code/springdemo/BeanScopeApp.java
@@ -0,0 +1,29 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeApp {
+
+	public static void main(String[] args) {
+		// load config xml
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		// retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+		//check if they are the same
+		
+		boolean result = (theCoach == alphaCoach);
+		System.out.println("are coachs same : "+result);
+		
+		System.out.println("Coach referance address        :" + theCoach);
+		System.out.println("Alpha coach referance address  :" + alphaCoach);
+
+		
+
+		
+		// close the context
+		context.close();
+
+	}
+
+}
