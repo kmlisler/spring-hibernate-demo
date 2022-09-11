c59111f2ef2bcc00a3ed340de857bc0d97e37a14
kmlisler <82888052+kmlisler@users.noreply.github.com>
2022-09-11 09:48:03 +0300

    setter and any method injection with @autowired

--- src/com/luv2code/springdemo/annotations/BasketballCoach.java
+++ src/com/luv2code/springdemo/annotations/BasketballCoach.java
@@ -7,16 +7,29 @@ import org.springframework.stereotype.Component;
 // if we dont give the bean id for component. default bean id will be basketballCoach
 @Component
 public class BasketballCoach implements Coach {	
+	
 	private FortuneService fortuneService;
 	
-	// spring will scan for a component that implements fortunservice interface
+	/*// constructor injection
+	 * spring will scan for a component that implements fortunservice interface
 	// bu örnekte HappyFortuneService bulunur ve BasketballCoach sınıfımıza inject edilir.
 	@Autowired
 	public BasketballCoach(FortuneService theFortuneService) {
 		fortuneService = theFortuneService;
+	}*/
+	
+	
+	// setter injection
+	@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("setter func setFortuneService BasketballCoach");
+		this.fortuneService = fortuneService;
 	}
-	
-	
+	// default const
+	public BasketballCoach() {
+		System.out.println("default const BasketballCoach");
+	}
+
 	@Override
 	public String getDailyWorkout() {
 		return "GO DUNK MA**";
